2e6bc2a2e51f393d9f94e8ad68bd405ccecd166c
Matias Ruiz Silva <ruizsilvamatias@gmail.com>
2023-04-07 21:11:32 -0300

    Changes of backgounds colors, id and class of proyects

--- scrip.js
+++ scrip.js
@@ -5,10 +5,10 @@ toggle.addEventListener("click", function () {
     this.classList.toggle("bi-moon");
     if (this.classList.toggle("bi-brightness-high-fill")) {
         body.style.background = "rgb(25, 23, 23)";
-        body.style.color = "white";
+        body.style.color = "rgb(125, 139, 168)";
         body.style.transition = "2s";
     } else {
-        body.style.background = "rgb(202, 202, 202)";
+        body.style.background = "rgb(125, 139, 168)";
         body.style.color = "black";
         body.style.transition = "2s";
     }
